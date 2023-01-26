f6b6e71250602b193fcb112ba0696012acd511b8
IvanLarionov90 <gnosticism100500@gmail.com>
2023-01-26 15:35:18 +0500

    use some TK func

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
-import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware} from 'redux'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
-import thunk from 'redux-thunk';
 
 
 const stringMiddleWare = () => (next) => (action) => {
@@ -13,10 +12,10 @@ const stringMiddleWare = () => (next) => (action) => {
     return next(action)
 }
 
-const store = createStore(
-                        combineReducers({heroes, filters}),
-                        compose(applyMiddleware(thunk, stringMiddleWare),
-                                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                        ));
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleWare),
+    devTools: process.env.NODE_ENV !== 'production'
+})
 
 export default store;
